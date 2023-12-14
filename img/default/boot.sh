6ce3ae9256f7be0a53c985bba270220e83265ad1
Matt Struble <mattstruble@outlook.com>
2023-12-14 10:59:53 -0500

    fix: move wwan creation and update radio1 configuration
    Signed-off-by: Matt Struble <mattstruble@outlook.com>

--- img/default/boot.sh
+++ img/default/boot.sh
@@ -5,7 +5,7 @@ lan_ip_address="192.168.1.1"
 
 # TODO: Find more reliable way to list wifi devices
 wifi_count=$(lsusb | wc -l)
-eth_count=$(nmcli device | grep eth\  | wc -l)
+#eth_count=$(nmcli device | grep eth  | wc -l)
 
 # log potential errors
 exec >/tmp/setup.log 2>&1
@@ -18,6 +18,13 @@ uci set network.lan.netmask="255.255.255.0"
 uci set network.lan.device="br-lan"
 uci set network.lan.ip6assign="60"
 uci set network.lan.force_link="1"
+uci commit network
+
+# Configure WWAN
+uci set network.wwan="interface"
+uci set network.wwan.proto="dhcp"
+uci set network.wwan.peerdns="0"
+uci set network.wwan.dns="9.9.9.9 1.1.1.1"
 uci commit network
 
 # Configure WLAN
@@ -34,17 +41,16 @@ uci commit wireless
 # Configure secondary wifi-iface as the client to connect to the external Wifi AP
 # This is based on the assumption that the usb device will have longer range than the built in rpi
 if [ "$wifi_count" -gt 1 ]; then
-    uci set network.wwan="interface"
-    uci set network.wwan.proto="dhcp"
-    uci set network.wwan.peerdns="0"
-    uci set network.wwan.dns="9.9.9.9 1.1.1.1"
-    uci commit network
+    uci set wireless.radio1='wifi-device'
+    uci set wireless.radio1.channel='36'
+    uci set wireless.radio1.disabled='0'
 
-    uci set wireless.@wifi-device[1].disabled='0'
     uci set wireless.@wifi-iface[1].disabled='0'
+    uci set wireless.@wifi-iface[1].device='radio1'
     uci set wireless.@wifi-iface[1].mode="client"
     uci set wireless.@wifi-iface[1].network="wwan"
     uci commit wireless
 
+    wifi
     ifconfig wlan1 up
 fi
