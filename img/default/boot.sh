31d1196fd7462b4b83116b285183c4ae24e5185f
Matt Struble <mattstruble@outlook.com>
2023-12-19 15:00:10 -0500

    feat(default): wlan via wifi setup (#2)
    * feat(default): wlan via wifi setup
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * ci: remove quotes around default
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * Revert "ci: remove quotes around default"
    
    This reverts commit cf5bba857d9069e2ba6c0e7f5ce46d6eeba6ded3.
    
    * ci: fromJson
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * ci: typo
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * ci: toJSon
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * ci(fetch-openwrt-image): rely on toJSON
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * feat(boot): wifi encryption to WPA3/WPA2 mixed
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * docs: add more acknowledgements and update todo
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * feat(default): add bridge lan and secondary wifi
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * chore: change encryption back to psk2 for testing
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * feat: add network-manager
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: wwan interface
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: temporary workaround for counting wifi devices
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: move wwan creation and update radio1 configuration
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * ci(openwrt): double wait time
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: delete network manager
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: wifi boot
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: delete panda specific chip info
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: add mt7601 firmware
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: more usb firmware
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * feat: shotgun usb firmware
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: remove unsupported packages
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: set the interfaces devices explicitly
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * ci: deprecate rpi-1
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: remove wifi and ifconfig commands
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * fix: use explicit names instead of index
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    * docs: update todo
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>
    
    ---------
    
    Signed-off-by: Matt Struble <mattstruble@outlook.com>

---
+++ img/default/boot.sh
@@ -0,0 +1,56 @@
+wlan_name="OpenWRT"
+wlan_password="ChangeMe123456"
+
+lan_ip_address="192.168.1.1"
+
+# TODO: Find more reliable way to list wifi devices
+wifi_count=$(lsusb | wc -l)
+#eth_count=$(nmcli device | grep eth  | wc -l)
+
+# log potential errors
+exec >/tmp/setup.log 2>&1
+
+# Configure LAN
+# More options: https://openwrt.org/docs/guide-user/base-system/basic-networking
+uci set network.lan.ipaddr="$lan_ip_address"
+uci set network.lan.proto="static"
+uci set network.lan.netmask="255.255.255.0"
+uci set network.lan.device="br-lan"
+uci set network.lan.ip6assign="60"
+uci set network.lan.force_link="1"
+uci commit network
+
+# Configure WWAN
+uci set network.wwan="interface"
+uci set network.wwan.proto="dhcp"
+uci set network.wwan.peerdns="0"
+uci set network.wwan.dns="9.9.9.9 1.1.1.1"
+uci commit network
+
+# Configure WLAN
+# More options: https://openwrt.org/docs/guide-user/network/wifi/basic#wi-fi_interfaces
+uci set wireless.radio0.disabled='0'
+uci set wireless.default_radio0.disabled='0'
+uci set wireless.default_radio0.device="radio0"
+uci set wireless.default_radio0.encryption='psk2'
+uci set wireless.default_radio0.ssid="$wlan_name"
+uci set wireless.default_radio0.key="$wlan_password"
+uci set wireless.default_radio0.mode="ap"
+uci set wireless.default_radio0.network="lan"
+uci commit wireless
+
+# Configure secondary wifi-iface as the client to connect to the external Wifi AP
+# This is based on the assumption that the usb device will have longer range than the built in rpi
+if [ "$wifi_count" -gt 1 ]; then
+    uci set wireless.radio1.disabled='0'
+    uci set wireless.radio1.channel='auto'
+    # uci set wireless.radio1='wifi-device'
+    # uci set wireless.radio1.channel='36'
+    # uci set wireless.radio1.disabled='0'
+
+    uci set wireless.default_radio1.disabled='0'
+    uci set wireless.default_radio1.device='radio1'
+    uci set wireless.default_radio1.mode="client"
+    uci set wireless.default_radio1.network="wwan"
+    uci commit wireless
+fi
