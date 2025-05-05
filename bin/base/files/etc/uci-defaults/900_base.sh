7b84aba14e860d4fb2228c09095d1d70476b5e61
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-05-05 17:26:45 -0400

    feat: automatically configure all plugged in usbs (#17)

--- bin/base/files/etc/uci-defaults/900_base.sh
+++ bin/base/files/etc/uci-defaults/900_base.sh
@@ -1,12 +1,8 @@
+#!/bin/bash
 wlan_name="OpenWRT"
 wlan_password="ChangeMe123456"
 wlan_encryption="psk2"
-
-# TODO: Randomly generate?
 lan_ip_address="10.71.71.1"
-
-# https://unix.stackexchange.com/a/552995
-wifi_count=$(ls /sys/class/ieee80211/*/device/net/* -d | wc -l)
 
 # log potential errors
 exec >/tmp/setup.log 2>&1
@@ -14,8 +10,7 @@ exec >/tmp/setup.log 2>&1
 # Configure LAN
 # More options: https://openwrt.org/docs/guide-user/base-system/basic-networking
 uci -q delete network.lan
-uci add network lan
-uci set network.lan=interface
+uci set network.lan="interface"
 uci set network.lan.ipaddr="$lan_ip_address"
 uci set network.lan.proto="static"
 uci set network.lan.netmask="255.255.255.0"
@@ -25,13 +20,12 @@ uci set network.lan.force_link="1"
 uci commit network
 
 # Configure WWAN
-uci -q delete network.wwan
-uci add network.wwan
-uci set network.wwan=interface
-uci set network.wwan.proto="dhcp"
-uci set network.wwan.peerdns="0"
+uci -q delete network.wan
+uci set network.wan="interface"
+uci set network.wan.proto="dhcp"
+uci set network.wan.peerdns="1"
 # Set DNS to Quad9 and Cloudflare Secure
-uci set network.wwan.dns="9.9.9.9 149.112.112.112 1.1.1.2 1.0.0.2"
+# uci set network.wwan.dns="9.9.9.9 149.112.112.112 1.1.1.2 1.0.0.2"
 uci commit network
 
 #### Configure Firewall ####
@@ -49,7 +43,7 @@ uci commit firewall
 # WAN
 uci add firewall zone
 uci set firewall.@zone[-1].name='wan'
-uci set firewall.@zone[-1].network='wwan'
+uci set firewall.@zone[-1].network='wan'
 uci set firewall.@zone[-1].input='ACCEPT'
 uci set firewall.@zone[-1].output='ACCEPT'
 uci set firewall.@zone[-1].forward='REJECT'
@@ -60,15 +54,15 @@ uci commit firewall
 #### Configure Travelmate ####
 # More options: https://github.com/openwrt/packages/blob/master/net/travelmate/files/README.md
 ####
-uci set travelmate.global="travelmate"
-uci set travelmate.global.trm_enabled="0"
-uci set travelmate.global.trm_iface="wwan"
-uci set travelmate.global.trm_captive="1"
-uci set travelmate.global.trm_netcheck="1"
-uci set travelmate.global.trm_proactive="1"
-uci set travelmate.global.trm_autoadd="1"
-uci set travelmate.global.trm_randomize="1"
-uci commit travelmate
+# uci set travelmate.global="travelmate"
+# uci set travelmate.global.trm_enabled="0"
+# uci set travelmate.global.trm_iface="wwan"
+# uci set travelmate.global.trm_captive="1"
+# uci set travelmate.global.trm_netcheck="1"
+# uci set travelmate.global.trm_proactive="1"
+# uci set travelmate.global.trm_autoadd="1"
+# uci set travelmate.global.trm_randomize="1"
+# uci commit travelmate
 
 #### Configure WLAN ####
 # More options: https://openwrt.org/docs/guide-user/network/wifi/basic#wi-fi_interfaces
@@ -80,6 +74,11 @@ pi_path=$(find /sys/devices/platform/soc/*mmc*/ -name "net" | xargs dirname)
 
 uci set wireless.radio0.disabled='0'
 uci set wireless.radio0.path="${pi_path##/sys/devices/}"
+
+#### Configure wireless interfaces
+###
+
+# Set up clear access point
 uci set wireless.default_radio0.disabled='0'
 uci set wireless.default_radio0.device="radio0"
 uci set wireless.default_radio0.encryption="$wlan_encryption"
@@ -89,31 +88,4 @@ uci set wireless.default_radio0.mode="ap"
 uci set wireless.default_radio0.network="lan"
 uci commit wireless
 
-# Configure secondary wifi-iface as the client to connect to the external Wifi AP
-# This is based on the assumption that the usb device will have longer range than the built in rpi
-if [ "$wifi_count" -gt 1 ]; then
-    # Hardcode the usb device to radio1
-    # https://forum.openwrt.org/t/list-option-paths-usb-radio-firstboot/96436
-    usb_path=$(find /sys/devices/platform/soc/*usb*/ -name "net" | xargs dirname)
-
-    uci set wireless.radio1.disabled='1'
-    uci set wireless.radio1.channel='auto'
-    uci set wireless.radio1.path="${usb_path##/sys/devices/}"
-    # uci set wireless.radio1='wifi-device'
-    # uci set wireless.radio1.channel='36'
-    # uci set wireless.radio1.disabled='0'
-
-    uci set wireless.default_radio1.disabled='1'
-    uci set wireless.default_radio1.device='radio1'
-    uci set wireless.default_radio1.mode="sta"
-    uci set wireless.default_radio1.network="wwan"
-    uci set wireless.default_radio1.encryption="$wlan_encryption"
-    uci set wireless.default_radio1.ssid="$wlan_name"
-    uci set wireless.default_radio1.key="$wlan_password"
-    uci commit wireless
-fi
-
-# Restart wifi devices
 wifi
-
-uci commit travelmate
--- bin/extras/usb/files/etc/uci-defaults/910_usb.sh
+++ bin/extras/usb/files/etc/uci-defaults/910_usb.sh
@@ -1,1 +1,21 @@
-echo "usb"
+#!/bin/bash
+
+radio_num=1
+# Iterate over all radios and set them up
+# https://stackoverflow.com/a/9612232
+while IFS= read -r -d '' line; do
+    device_dir=$(dirname "$line")
+
+    uci set wireless.radio"$radio_num".disabled='1'
+    uci set wireless.radio"$radio_num".channel='auto'
+    uci set wireless.radio"$radio_num.path=${device_dir##/sys/devices/}"
+
+    uci set wireless.default_radio"$radio_num".disabled='1'
+    uci set wireless.default_radio"$radio_num".mode='sta'
+    uci set wireless.default_radio"$radio_num".network="wan"
+
+    uci commit wireless
+
+    radio_num=$(expr "$radio_num" + 1)
+
+done < <(find /sys/devices/platform/soc/*usb*/ -name "net" -print0)
