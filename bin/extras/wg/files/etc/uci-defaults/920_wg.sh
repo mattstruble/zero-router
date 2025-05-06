ecd30a7dc4bbb8feb39ae4e669464744fd31741c
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-05-06 13:11:30 -0400

    feat: add wg lan zone
    Signed-off-by: Matt Struble <4325029+mattstruble@users.noreply.github.com>

--- bin/extras/wg/files/etc/uci-defaults/920_wg.sh
+++ bin/extras/wg/files/etc/uci-defaults/920_wg.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
-wlan_name="OpenWRT.enc"
-wlan_password="ChangeMe123456"
-wlan_encryption="psk2"
+wg_lan_ip_address="10.72.72.1"
 
 uci set network.wg0='interface'
 uci set network.wg0.proto='wireguard'
 
-uci set network.wg_network='interface'
-uci set network.wg_network.proto='none'
-uci set network.wg_network.ifname='wg0'
+uci set network.wg_lan="interface"
+uci set network.wg_lan.ipaddr="$wg_lan_ip_address"
+uci set network.wg_lan.proto="static"
+uci set network.wg_lan.netmask="255.255.255.0"
+uci set network.wg_lan.device="br-lan"
+uci set network.wg_lan.delegate='0'
+uci set network.wg_lan.defaultroute='0'
+uci commit network
+
+uci add firewall zone
+uci set firewall.@zone[-1].name='wg_lan_zone'
+uci set firewall.@zone[-1].network='wg_lan'
+uci set firewall.@zone[-1].input='DROP'
+uci set firewall.@zone[-1].output='DROP'
+uci set firewall.@zone[-1].forward='DROP'
+uci commit firewall
 
 uci add firewall zone
 uci set firewall.@zone[-1]='zone'
-uci set firewall.@zone[-1].name='wg'
-uci set firewall.@zone[-1].network='wg_network'
-uci set firewall.@zone[-1].input='ACCEPT'
-uci set firewall.@zone[-1].output='ACCEPT'
-uci set firewall.@zone[-1].forward='REJECT'
+uci set firewall.@zone[-1].name='wg0_zone'
+uci set firewall.@zone[-1].network='wg0'
+uci set firewall.@zone[-1].input='DROP'
+uci set firewall.@zone[-1].output='DROP'
+uci set firewall.@zone[-1].forward='DROP'
+uci set firewall.@zone[-1].masq='1'
+uci commit firewall
 
 uci add firewall forwarding
-uci set firewall.@forwarding[-1].src='wg'
-uci set firewall.@forwarding[-1].dest='wan'
+uci set firewall.@forwarding[-1].src='wg_lan_zone'
+uci set firewall.@forwarding[-1].dest='wg0_zone'
 
 uci commit network
 uci commit firewall
