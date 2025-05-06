d7cd65fedb98485e5403905c52210e97f1e92f85
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-05-06 13:49:07 -0400

    feat: allow established connections
    Signed-off-by: Matt Struble <4325029+mattstruble@users.noreply.github.com>

--- bin/extras/wg/files/etc/uci-defaults/920_wg.sh
+++ bin/extras/wg/files/etc/uci-defaults/920_wg.sh
@@ -17,7 +17,7 @@ uci add firewall zone
 uci set firewall.@zone[-1].name='wg_lan_zone'
 uci set firewall.@zone[-1].network='wg_lan'
 uci set firewall.@zone[-1].input='DROP'
-uci set firewall.@zone[-1].output='DROP'
+uci set firewall.@zone[-1].output='ACCEPT'
 uci set firewall.@zone[-1].forward='DROP'
 uci commit firewall
 
@@ -26,7 +26,7 @@ uci set firewall.@zone[-1]='zone'
 uci set firewall.@zone[-1].name='wg0_zone'
 uci set firewall.@zone[-1].network='wg0'
 uci set firewall.@zone[-1].input='DROP'
-uci set firewall.@zone[-1].output='DROP'
+uci set firewall.@zone[-1].output='ACCEPT'
 uci set firewall.@zone[-1].forward='DROP'
 uci set firewall.@zone[-1].masq='1'
 uci commit firewall
@@ -34,6 +34,14 @@ uci commit firewall
 uci add firewall forwarding
 uci set firewall.@forwarding[-1].src='wg_lan_zone'
 uci set firewall.@forwarding[-1].dest='wg0_zone'
+
+uci add firewall rule
+uci set firewall.@rule[-1].src='wg_lan_zone'
+uci set firewall.@rule[-1].dest='wg0_zone'
+uci set firewall.@rule[-1].proto='all'
+uci set firewall.@rule[-1].target='ACCEPT'
+uci set firewall.@rule[-1].family='ipv4'
+uci set firewall.@rule[-1].state='RELATED,ESTABLISHED'
 
 uci commit network
 uci commit firewall
